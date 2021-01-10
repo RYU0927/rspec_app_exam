df49e5ae8c8e7ada499dfa28dfdfdf14e01f3bbc
ryusuke.sato <jftrx029@gmail.com>
2021-01-10 16:28:15 +0900

    Add:traitを使ってstatus:done時の設定をまとめた

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,5 +6,11 @@ FactoryBot.define do
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
     association :project
+
+    trait :done do
+      status { :done }
+      project_id { project.id }
+      completion_date { Time.current.yesterday }
+    end
   end
 end
