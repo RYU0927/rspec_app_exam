9c0961479fa630efa1bb92743e26871b0eda2f91
ryusuke.sato <jftrx029@gmail.com>
2021-01-12 13:53:15 +0900

    Fatorybotの省略

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe 'Task', type: :system do
       end
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
-        done_task = FactoryBot.create(:task, :done)
+        done_task = create(:task, :done)
         visit edit_project_task_path(project, done_task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
